9d5829638831f53682989fb76d12fb2c31f667fa
Conzel <38732545+Conzel@users.noreply.github.com>
2022-04-15 16:39:15 +0200

    CHANGE: Massive speed up of transposed convolutions
    Instead of zero-padding the input and performing a normal convolution,
    we use the pytorch implementation of doing a "backwards pass" on the
    input. We calculate the channelwise-summed products of input and kernel,
    then sum the resulting patches where they overlap.

--- src/transposed_convolutions.rs
+++ src/transposed_convolutions.rs
@@ -5,11 +5,12 @@
 //! - <https://github.com/akutzer/numpy_cnn/blob/master/CNN/Layer/TransposedConv.py>
 //! - <https://ieee.nitk.ac.in/blog/deconv/>
 use crate::{
-    convolutions::{add_bias, get_padding_size, im2col_ref, ConvolutionLayer},
+    convolutions::{add_bias, ConvolutionLayer},
     ConvKernel, DataRepresentation, Padding,
 };
 use ndarray::*;
 use num_traits::Float;
+use std::ops::AddAssign;
 
 /// Analog to a Convolution Layer
 pub struct TransposedConvolutionLayer<F: Float> {
@@ -58,7 +59,63 @@ impl<F: 'static + Float + std::ops::AddAssign> TransposedConvolutionLayer<F> {
     }
 }
 
+/// Implementation of col2im as seen in the Pytorch cpp implementation.
+/// This implementation doesn't just assign the matrix patch-wise to a new
+/// matrix, but also sums the patches such that it unrolls the result
+/// of conv-transpose.
+/// mat is a matrix of size (image_width * image_height, c_out * k_h * k_w),
+/// in which each entry is the value of a kernel pixel multiplied with an image pixels
+/// with the channels summed out
+/// The output naming is a bit confusing, as this function is originally used in the backward
+/// pass. What this function returns is simply (channels, height, width).
+fn col2im_pt<'a, F: 'a + Float + AddAssign>(
+    data_col: &[F],
+    channels: usize,
+    height: usize,
+    width: usize,
+    output_height: usize,
+    output_width: usize,
+    kernel_h: usize,
+    kernel_w: usize,
+    pad_h: usize,
+    pad_w: usize,
+    stride: usize,
+) -> DataRepresentation<F> {
+    let mut res: Array1<F> = Array::zeros(channels * height * width);
+
+    let height_col = output_height;
+    let width_col = output_width;
+    let channels_col = channels * kernel_h * kernel_w;
+
+    for c_col in 0..channels_col {
+        let w_offset = c_col % kernel_w;
+        let h_offset = (c_col / kernel_w) % kernel_h;
+        let c_im = c_col / kernel_h / kernel_w;
+
+        for h_col in 0..height_col {
+            let h_im = (h_col * stride) as i64 - pad_h as i64 + h_offset as i64;
+
+            for w_col in 0..width_col {
+                let w_im = (w_col * stride) as i64 - pad_w as i64 + w_offset as i64;
+
+                if h_im < height as i64 && h_im >= 0 && w_im < width as i64 && w_im >= 0 {
+                    // TODO: Use unsafe get
+                    let el = res
+                        .get_mut((c_im * height + h_im as usize) * width + w_im as usize)
+                        .unwrap();
+
+                    // TODO: Use unsafe get
+                    el.add_assign(data_col[(c_col * height_col + h_col) * width_col + w_col]);
+                }
+            }
+        }
+    }
+    Array::from_shape_vec((channels, height, width), res.into_iter().collect()).unwrap()
+}
+
 /// Performs a transposed convolution on the input image. This upsamples the image.
+/// This implementation is identical to the supplied by Pytorch and works by performing
+/// a "backwards pass" on the input.
 ///
 /// More explanation can be read here:
 /// - <https://datascience.stackexchange.com/questions/6107/what-are-deconvolutional-layers>
@@ -76,154 +133,53 @@ impl<F: 'static + Float + std::ops::AddAssign> TransposedConvolutionLayer<F> {
 /// Returns:
 /// -----------------------------------------------
 /// - out: Output data, of shape (F, H', W')
-pub fn conv_transpose2d<'a, T, V, F: 'static + Float + std::ops::AddAssign>(
+pub fn conv_transpose2d<'a, T, V, F: 'static + Float + AddAssign>(
     kernel_weights: T,
     bias: Option<&Array1<F>>,
     im2d: V,
     padding: Padding,
     stride: usize,
-) -> Array3<F>
+) -> DataRepresentation<F>
 where
-    // This trait bound ensures that kernel and im2d can be passed as owned array or view.
-    // AsArray just ensures that im2d can be converted to an array view via ".into()".
-    // Read more here: https://docs.rs/ndarray/0.12.1/ndarray/trait.AsArray.html
     V: AsArray<'a, F, Ix3>,
     T: AsArray<'a, F, Ix4>,
 {
-    // Initialisations
-    let im2d_arr: ArrayView3<F> = im2d.into();
-    let kernel_weights_arr: ArrayView4<F> = kernel_weights.into();
-    let im2d_stride: Array3<F>;
-    let new_im_height: usize;
-    let new_im_width: usize;
-    let im_channel_stride: usize;
-    let im_height_stride: usize;
-    let im_width_stride: usize;
-
-    let weight_shape = kernel_weights_arr.shape();
-
-    let num_channels_out = weight_shape[0] as usize;
-    let num_filters = weight_shape[1] as usize;
-    let kernel_height = weight_shape[2] as usize;
-    let kernel_width = weight_shape[3] as usize;
-
-    // Dimensions: C, H, W
-    let im_channel = im2d_arr.len_of(Axis(0));
-    let im_height = im2d_arr.len_of(Axis(1));
-    let im_width = im2d_arr.len_of(Axis(2));
-
-    // Calculate output shapes H', W'
-    // https://towardsdatascience.com/understand-transposed-convolutions-and-build-your-own-transposed-convolution-layer-from-scratch-4f5d97b2967
-    // https://theano-pymc.readthedocs.io/en/latest/tutorial/conv_arithmetic.html
-    // H' =  (H - 1) * stride  + HH
-    // W' =  (W - 1) * stride  + WW
-    new_im_height = (im_height - 1) * stride + kernel_height;
-    new_im_width = (im_width - 1) * stride + kernel_width;
-
-    // weights.reshape(F, HH*WW*C)
-    // loop over F and C ; flip the kernel and append it back
-    let mut filter_col: Array3<F> =
-        Array::zeros((num_channels_out, num_filters, kernel_height * kernel_width));
-    for i in 0..num_channels_out {
-        for j in 0..num_filters {
-            let patch_kernel_weights = kernel_weights_arr.slice(s![i, j, .., ..]);
-            let mut weights_flatten = patch_kernel_weights
-                .into_shape(kernel_height * kernel_width)
-                .unwrap()
-                .to_vec();
-            //FLIP
-            weights_flatten.reverse();
-            let weights_reverse =
-                Array::from_shape_vec(kernel_height * kernel_width, weights_flatten).unwrap();
-            filter_col
-                .slice_mut(s![i, j, 0..kernel_height * kernel_width])
-                .assign(&weights_reverse);
-        }
-    }
-    filter_col.swap_axes(0, 1);
-    let target_shape = (num_filters, num_channels_out * kernel_height * kernel_width);
-    let filter_col_flatten =
-        Array::from_shape_vec(target_shape, filter_col.iter().map(|a| *a).collect()).unwrap();
-
-    // STRIDE > 1
-    if stride != 1 {
-        // https://github.com/akutzer/numpy_cnn/blob/master/CNN/Layer/TransposedConv.py
-        // ASSUMPTION: stride[0] == stride[1]
-        let stride_h = stride * im_height;
-        let stride_w = stride * im_width;
-        let mut im2d_stride_full: Array3<F> = Array::zeros((im_channel, stride_h, stride_w));
-        im2d_stride_full
-            .slice_mut(s![.., ..;stride, ..;stride])
-            .assign(&im2d_arr);
-        let stride_h_crop = (stride_h - stride) + 1;
-        let stride_w_crop = (stride_w - stride) + 1;
-        im2d_stride = im2d_stride_full
-            .slice_mut(s![.., ..stride_h_crop, ..stride_w_crop])
-            .into_owned();
-    } else {
-        im2d_stride = im2d_arr.into_owned();
+    let kernel_mat: ArrayView4<F> = kernel_weights.into();
+    let im_mat: ArrayView3<F> = im2d.into();
+    // kernel has shape (C, F, HH, WW)
+    let k_c = kernel_mat.shape()[0];
+    let k_f = kernel_mat.shape()[1];
+    let k_h = kernel_mat.shape()[2];
+    let k_w = kernel_mat.shape()[3];
+    let ker_reshape = kernel_mat.into_shape((k_c, k_f * k_h * k_w)).unwrap();
+    let im_c = im_mat.shape()[0];
+    let im_h = im_mat.shape()[1];
+    let im_w = im_mat.shape()[2];
+    let im_reshape = im_mat.into_shape((im_c, im_h * im_w)).unwrap();
+    let data_matrix: Array2<F> = im_reshape.t().dot(&ker_reshape);
+    // output padding might be necessary to achieve the desired shape
+    let (pad_h, pad_w, output_pad_h, output_pad_w) = match padding {
+        Padding::Valid => (0, 0, 0, 0),
+        Padding::Same => (k_h / 2, k_w / 2, stride - 1, stride - 1),
     };
-
-    im_channel_stride = im2d_stride.len_of(Axis(0));
-    im_height_stride = im2d_stride.len_of(Axis(1));
-    im_width_stride = im2d_stride.len_of(Axis(2));
-
-    // PADDING
-    // fn:im2col() for with padding always
-    let pad_h = kernel_height - 1;
-    let pad_w = kernel_width - 1;
-    let mut im2d_arr_pad: Array3<F> = Array::zeros((
-        im_channel_stride,
-        im_height_stride + pad_h + pad_h,
-        im_width_stride + pad_w + pad_w,
-    ));
-    let pad_int_h = im_height_stride + pad_h;
-    let pad_int_w = im_width_stride + pad_w;
-    // https://github.com/rust-ndarray/ndarray/issues/823
-    im2d_arr_pad
-        .slice_mut(s![.., pad_h..pad_int_h, pad_w..pad_int_w])
-        .assign(&im2d_stride);
-
-    let im_height_pad = im2d_arr_pad.len_of(Axis(1));
-    let im_width_pad = im2d_arr_pad.len_of(Axis(2));
-
-    let im_col = im2col_ref(
-        im2d_arr_pad.view(),
-        kernel_height,
-        kernel_width,
-        im_height_pad,
-        im_width_pad,
-        im_channel,
-        1,
+    println!("{:?}, {:?}", pad_h, pad_w);
+    let output_height = (im_h - 1) * stride + k_h + output_pad_h - 2 * pad_h;
+    let output_width = (im_w - 1) * stride + k_w + output_pad_w - 2 * pad_w;
+    // let output_height = (im_h - 1) * stride + k_h;
+    // let output_width = (im_w - 1) * stride + k_w;
+    let data_matrix_contiguous = Array::from_iter(data_matrix.view().t().iter().map(|a| *a));
+    let t_conv_output = col2im_pt(
+        data_matrix_contiguous.as_slice().unwrap(),
+        k_f,
+        output_height,
+        output_width,
+        im_h,
+        im_w,
+        k_h,
+        k_w,
+        pad_h,
+        pad_w,
+        stride,
     );
-    // NOTE: The kernel strides across the image at 1 regardless of the stride we provide
-
-    let filter_transpose = filter_col_flatten.t();
-    let mul = im_col.dot(&filter_transpose); // + bias_m
-
-    let mut mul_reshape = mul
-        .into_shape((new_im_height, new_im_width, num_filters))
-        .unwrap()
-        .into_owned();
-    mul_reshape.swap_axes(0, 2);
-    mul_reshape.swap_axes(1, 2);
-
-    let output = if padding == Padding::Same {
-        let (_, _, pad_top, pad_bottom, pad_left, pad_right) = get_padding_size(
-            im_height * stride,
-            im_width * stride,
-            stride,
-            kernel_height,
-            kernel_width,
-        );
-
-        let pad_right_int = new_im_width - pad_right;
-        let pad_bottom_int = new_im_height - pad_bottom;
-        mul_reshape
-            .slice(s![.., pad_top..pad_bottom_int, pad_left..pad_right_int])
-            .into_owned()
-    } else {
-        mul_reshape.into_owned()
-    };
-    add_bias(&output, bias)
+    return add_bias(&t_conv_output, bias);
 }
